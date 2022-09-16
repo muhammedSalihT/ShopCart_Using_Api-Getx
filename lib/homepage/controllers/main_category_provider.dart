09dfee768c9c361233f357309aa516dabc7dede3
Salih T <mhdsalih9656@gmail.com>
2022-09-16 14:45:08 +0530

    main_category_controller Added

--- lib/homepage/controllers/main_category_provider.dart
+++ lib/homepage/controllers/main_category_provider.dart
@@ -1,15 +1,13 @@
-import 'package:flutter/cupertino.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:project_shopcart/homepage/models/main_category_model.dart';
 
-class MainCategoryProvider extends ChangeNotifier{
-
+class MainCategoryProvider extends GetxController {
   List<MainCategoryModel> mainCategory = [
-  MainCategoryModel(categoryName: "Shirts"),
-  MainCategoryModel(categoryName: "Jeans"),
-  MainCategoryModel(categoryName: "Tracksuits"),
-  MainCategoryModel(categoryName: "Smart Watches"),
-  MainCategoryModel(categoryName: "Shoes"),
-  MainCategoryModel(categoryName: "Wallets"),
-];
+    MainCategoryModel(categoryName: "Shirts"),
+    MainCategoryModel(categoryName: "Jeans"),
+    MainCategoryModel(categoryName: "Tracksuits"),
+    MainCategoryModel(categoryName: "Smart Watches"),
+    MainCategoryModel(categoryName: "Shoes"),
+    MainCategoryModel(categoryName: "Wallets"),
+  ];
 }
-
