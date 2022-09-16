3cb5505c81e45e25454829911a77f028cb24396a
Salih T <mhdsalih9656@gmail.com>
2022-09-16 14:18:31 +0530

    main category model created

---
+++ lib/homepage/models/main_category_model.dart
@@ -0,0 +1,14 @@
+class MainCategoryModel {
+  final String categoryName;
+
+  MainCategoryModel({required this.categoryName});
+}
+
+List<MainCategoryModel> mainCategory = [
+  MainCategoryModel(categoryName: "Shirts"),
+  MainCategoryModel(categoryName: "Jeans"),
+  MainCategoryModel(categoryName: "Tracksuits"),
+  MainCategoryModel(categoryName: "Smart Watches"),
+  MainCategoryModel(categoryName: "Shoes"),
+  MainCategoryModel(categoryName: "Wallets"),
+];
