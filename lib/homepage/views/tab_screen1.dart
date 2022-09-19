506b2a0a081052339fd54308c8a961475e6cb5f5
Salih T <mhdsalih9656@gmail.com>
2022-09-19 17:46:34 +0530

    tab1 added

--- lib/homepage/views/tab_screen1.dart
+++ lib/homepage/views/tab_screen1.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class TabScreen1 extends StatelessWidget {
   const TabScreen1({Key? key}) : super(key: key);
