4ccef6d11fc0dadf71c8cd5835098410f7a0951c
Salih T <mhdsalih9656@gmail.com>
2022-09-19 19:57:19 +0530

    home editted

--- lib/homepage/controllers/main_category_controller.dart
+++ lib/homepage/controllers/main_category_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_rx/get_rx.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:project_shopcart/homepage/models/main_category_model.dart';
 import 'package:project_shopcart/homepage/views/global_tab.dart';
@@ -18,19 +17,18 @@ class MainCategoryController extends GetxController {
     MainCategoryModel(categoryName: "Wallets"),
   ];
 
-
-  Widget getScreen(String screen){
-  switch(screen){
-    case "Shirts":
-    return const TabScreen1();
-    case "Jeans":
-    return const TabScreen2();
-    case "Tracksuits":
-    return const TabScreen3();
-    case "Smart Watches":
-    return const TabScreen4();
-    default :return const GlobalTab();
+  Widget getScreen(String screen) {
+    switch (screen) {
+      case "Shirts":
+        return const TabScreen1();
+      case "Jeans":
+        return const TabScreen2();
+      case "Tracksuits":
+        return const TabScreen3();
+      case "Smart Watches":
+        return const TabScreen4();
+      default:
+        return const GlobalTab();
+    }
   }
-  
-}
 }
