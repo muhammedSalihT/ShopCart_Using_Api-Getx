cb6072c89f44a2b5fb9f859cc6c3d4c0d38c297e
Salih T <mhdsalih9656@gmail.com>
2022-09-19 17:49:40 +0530

    tab3 added

---
+++ lib/homepage/views/tab_screen3.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabScreen3 extends StatelessWidget {
+  const TabScreen3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
+}
