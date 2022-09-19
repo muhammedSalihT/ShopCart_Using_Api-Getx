39c337de4a333b4d51a9595b55bcb2932222242d
Salih T <mhdsalih9656@gmail.com>
2022-09-19 17:47:49 +0530

    tab2 added

---
+++ lib/homepage/views/tab_screen2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabScreen2 extends StatelessWidget {
+  const TabScreen2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
+}
