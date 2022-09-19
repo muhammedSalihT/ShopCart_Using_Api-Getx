c1d14f9bda17ff3419c88a10a2851683d2a543b9
Salih T <mhdsalih9656@gmail.com>
2022-09-19 17:53:28 +0530

    Global Tab

---
+++ lib/homepage/views/global_tab.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GlobalTab extends StatelessWidget {
+  const GlobalTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
+}
