6a9efc424a8b2dc363f10686d67cc2b363a63bfa
Salih T <mhdsalih9656@gmail.com>
2022-09-19 17:51:00 +0530

    tab4 added

---
+++ lib/homepage/views/tab_screen4.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabScreen4 extends StatelessWidget {
+  const TabScreen4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
+}
