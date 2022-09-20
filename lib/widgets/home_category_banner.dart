745f74a5038b789ba0a4a405d7c9f4bce921952f
Salih T <mhdsalih9656@gmail.com>
2022-09-20 17:42:00 +0530

    home category banner added

--- lib/widgets/home_category_banner.dart
+++ lib/widgets/home_category_banner.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 class HomeCategoryBanner extends StatelessWidget {
   const HomeCategoryBanner({
     Key? key,
+    required this.text,
   }) : super(key: key);
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return SizedBox(
       height: size.height * .05,
-      child: const Padding(
-        padding: EdgeInsets.only(top: 10.0, left: 5),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0, left: 5),
         child: Text(
-          "Recently Viewed Items",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          text,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
         ),
       ),
     );
