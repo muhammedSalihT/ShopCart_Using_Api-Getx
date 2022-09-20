491e4afe79a55dff4f998dcc6906cc0e0c980e47
Salih T <mhdsalih9656@gmail.com>
2022-09-20 17:36:36 +0530

    home category banner added

--- lib/homepage/views/tab_screen1.dart
+++ lib/homepage/views/tab_screen1.dart
@@ -72,23 +72,3 @@ class TabScreen1 extends StatelessWidget {
   }
 }
 
-class HomeCategoryBanner extends StatelessWidget {
-  const HomeCategoryBanner({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * .05,
-      child: const Padding(
-        padding: EdgeInsets.only(top: 10.0, left: 5),
-        child: Text(
-          "Recently Viewed Items",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-        ),
-      ),
-    );
-  }
-}
