612cd19eb5664a85db5012c6688f7c57d41a31dd
Salih T <mhdsalih9656@gmail.com>
2022-09-21 17:36:38 +0530

    auth screen added

--- lib/widgets/custom_textform.dart
+++ lib/widgets/custom_textform.dart
@@ -4,37 +4,38 @@ class CustomTextForm extends StatelessWidget {
   const CustomTextForm({
     Key? key,
     this.width,
+    this.validator,
+    this.keybordType = TextInputType.text,
+    this.icon,
+    this.hintText,
   }) : super(key: key);
+
   final double? width;
+  final FormFieldValidator<String>? validator;
+  final TextInputType keybordType;
+  final IconData? icon;
+  final String? hintText;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 8.0,
+      elevation: 3.0,
       color: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(25),
       ),
-      child: SizedBox(
-        width: (width == null) ? 300 : width,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                style: const TextStyle(color: Colors.black),
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  icon: Icon(
-                    Icons.message,
-                    color: Colors.black,
-                  ),
-                  hintText: "Email Address",
-                ),
-              ),
-            ),
-          ],
+      child: TextFormField(
+        validator: validator,
+        keyboardType: keybordType,
+        style: const TextStyle(color: Colors.black),
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(
+                color: Colors.orangeAccent.withOpacity(0.5), width: 2),
+          ),
+          hintText: hintText,
         ),
       ),
     );
