eb330c69c17cf6da4b9cb89bfd9f3c96d985c21c
Salih T <mhdsalih9656@gmail.com>
2022-09-25 18:23:11 +0530

    Color const added

--- lib/constends/colors.dart
+++ lib/constends/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/cupertino.dart';
 
 class ConstColor {
   final Color whiteColor = const Color.fromARGB(255, 199, 202, 205);
+  final Color brightWhite = const Color.fromARGB(255, 223, 234, 245);
 }
