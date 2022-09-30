defb62181924bcd8576baca943beb5a37f386d02
Salih T <mhdsalih9656@gmail.com>
2022-09-30 11:56:34 +0530

    main categorie added

--- lib/api_const/api_urls.dart
+++ lib/api_const/api_urls.dart
@@ -2,4 +2,6 @@ class Url {
   static String baseUrl = "http://10.0.2.2:3000/";
   static String signUp = "account/signup-email";
   static String verification = "account/verify-email-otp";
+  static String baseUrl2 = "http://10.0.2.2:4000/";
+  static String mainCategorie = "api/getAllMainCategories";
 }
