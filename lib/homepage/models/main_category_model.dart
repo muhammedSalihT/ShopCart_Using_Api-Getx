39404deec6fc38aed278cf15cec7d6027ceabd36
Salih T <mhdsalih9656@gmail.com>
2022-10-02 14:19:56 +0530

    model editted

--- lib/homepage/models/main_category_model.dart
+++ lib/homepage/models/main_category_model.dart
@@ -1,7 +1,35 @@
-class MainCategoryModel {
-  final String categoryName;
+class MainCategorieModel {
+  MainCategorieModel({this.data, this.messege});
 
-  MainCategoryModel({required this.categoryName});
+  List<GetAllCategories>? data;
+  String? messege;
+
+  factory MainCategorieModel.fromJson(Map<String, dynamic> json) =>
+      MainCategorieModel(
+        data: json["data"] == null
+            ? []
+            : List<GetAllCategories>.from(
+                json["data"].map((x) => GetAllCategories.fromJson(x))),
+      );
 }
 
+class GetAllCategories {
+  GetAllCategories({
+    this.id,
+    this.mainProduct,
+  });
 
+  String? id;
+  String? mainProduct;
+
+  factory GetAllCategories.fromJson(Map<String, dynamic> json) =>
+      GetAllCategories(
+        id: json["_id"],
+        mainProduct: json["mainProduct"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "mainProduct": mainProduct,
+      };
+}
