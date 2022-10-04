2796f16b8d412f2da2cf90b7ab515b124c3567d3
Salih T <mhdsalih9656@gmail.com>
2022-10-04 11:52:14 +0530

    api url updated

--- lib/api_const/api_urls.dart
+++ lib/api_const/api_urls.dart
@@ -1,6 +1,7 @@
 class Url {
   static String baseUrl = "http://10.0.2.2:3000/";
   static String signUp = "api/signup";
+  static String signIn = "api/signIn";
   static String verification = "account/verify-email-otp";
   static String mainCategorie = "api/getAllMainCategories";
 }
