80c56e1d246375fe82e5cdb2a6cd2e09ef27eeea
Salih T <mhdsalih9656@gmail.com>
2022-10-08 17:06:38 +0530

    editted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:project_shopcart/onbording_pages/view/onbording_screens_view.dart';
+import 'package:get/get.dart';
+import 'package:project_shopcart/splash_screen/views/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,18 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        floatingActionButtonTheme:
-            const FloatingActionButtonThemeData(backgroundColor: Colors.black),
-        primarySwatch: Colors.blue,
-      ),
-      home: OnBordingScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.black),
+          primarySwatch: Colors.blue,
+        ),
+        home: SpalshScreen());
   }
 }
