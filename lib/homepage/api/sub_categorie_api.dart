6cf9acc08a723686b6cbd31a6ef268b0f34092cd
Salih T <mhdsalih9656@gmail.com>
2022-10-11 10:58:38 +0530

    sub categorie controller editted

--- lib/homepage/api/sub_categorie_api.dart
+++ lib/homepage/api/sub_categorie_api.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:project_shopcart/api_const/api_urls.dart';
 import 'package:project_shopcart/homepage/models/sub_categorie_model.dart';
@@ -10,21 +9,22 @@ class SubCategorieApi {
     try {
       final responce =
           await Dio().get(Url.baseUrl + Url.subCategorie + categorieId);
-      log('message');
+      log(Url.baseUrl + Url.subCategorie + categorieId);
+      log(responce.toString());
       log(Url.baseUrl + Url.subCategorie + categorieId);
       if (responce.statusCode == 200) {
         return SubCategorieModel.fromJson(responce.data);
       } else {
-        return SubCategorieModel(messege: "Unknown error occuired");
+        return SubCategorieModel(msg: "Check your connectivity ");
       }
     } on DioError catch (e) {
       log('dio error');
       log(e.response!.data.toString());
-      return SubCategorieModel(messege: e.response!.data.toString());
+      return SubCategorieModel(msg: e.response!.data.toString());
     } catch (e) {
       print('catch error');
       print(e.toString());
-      return SubCategorieModel(messege: e.toString());
+      return SubCategorieModel(msg: e.toString());
     }
   }
 }
